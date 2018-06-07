9babe34dfee975573bbdf4d3b502209c83399cf6
OscarTonusco <oscare@utp.edu.co>
2018-06-07 13:50:13 -0500

    Des

--- ProyectoLabSoft/config/Migrations/20180606033741_CreateControlDeProduccion.php
+++ ProyectoLabSoft/config/Migrations/20180606033741_CreateControlDeProduccion.php
@@ -1,7 +1,7 @@
 <?php
 use Migrations\AbstractMigration;
 
-class CreateControlManObra extends AbstractMigration
+class CreateControlDeProduccion extends AbstractMigration
 {
     /**
      * Change Method.
